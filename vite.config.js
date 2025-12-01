b3b280d2a14088e9bc3f3bb736c11e3adf8a97b7
AlejandroFer1 <alumno.728382@ies-azarquiel.es>
2025-12-01 19:19:58 +0100

    Usar rutas relativas

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,8 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    // Reemplaza 'nombre-de-tu-repo' con el nombre exacto de tu repositorio en GitHub
-    // Por ejemplo, si tu repo es https://github.com/usuario/mi-proyecto, usa '/mi-proyecto/'
-    base: '/P4.1-Galer-a-responsiva-eficiente/',
+    // Usamos ruta relativa para evitar problemas con el nombre del repo
+    base: './',
     build: {
         outDir: 'docs',
     },
